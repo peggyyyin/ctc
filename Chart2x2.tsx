99e7347f2b934205480911dd26bbb7ec094a0b94
peggyyyin <86021734+peggyyyin@users.noreply.github.com>
2025-01-28 23:01:50 +0000

    dot fix

--- Chart2x2.tsx
+++ Chart2x2.tsx
@@ -80,8 +80,8 @@ const Chart2x2: React.FC<Chart2x2Props> = ({ xSum, ySum }) => {
         // Plot result
         ctx.fillStyle = "#40c7cc";
         ctx.beginPath();
-        const x = (xSum / 12 + 1) * (canvas.width / 2);
-        const y = (1 - ySum / 12) * (canvas.height / 2);
+        const x = (xSum / 12 + 1) * (canvas.width / 2) * .9;
+        const y = (1 - ySum / 12) * (canvas.height / 2 * .9;
         ctx.arc(x, y, 8, 0, 2 * Math.PI);
         ctx.fill();
       }
@@ -93,7 +93,7 @@ const Chart2x2: React.FC<Chart2x2Props> = ({ xSum, ySum }) => {
       ref={canvasRef}
       width={400} // Canvas size
       height={400}
-      className="border-2 border-white rounded-lg"
+      className="border-2 border-transparent rounded-lg"
     />
   );
 };

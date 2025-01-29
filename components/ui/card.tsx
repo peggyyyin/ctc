9e17ecd533a7f9a24c348e171aa08141b7d9106c
peggyyyin <86021734+peggyyyin@users.noreply.github.com>
2025-01-29 01:15:26 +0000

    radio

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -22,6 +22,19 @@ const Card = React.forwardRef<
     }}
     {...props}
   />
+
+  const CardDescription = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+CardDescription.displayName = "CardDescription"
+
 ));
 Card.displayName = "Card";
 
